00515d9604c776f5d7be50fbf7f3f81c5e48dcab
Kaylotura@gmail.com <Kaylotura@gmail.com>
2018-05-16 13:10:32 -0700

    weighted pronouns based on gender article

--- gender_generator.js
+++ gender_generator.js
@@ -1,13 +1,18 @@
 var gender_descriptors = [
   '', 'androgynous', 'butch', 'bigender', 'demi', 'femme', 'fluid', 'hairy',
-  'masculine', 'non-binary', 'non-conforming', 'pangender', 'sparkle', 'spooky',
+  'hard', 'masculine', 'non-binary', 'non-conforming', 'pangender', 'sparkle',
   'trans',
 ];
 // TODO: 'bigender' and 'pangender' tags to have 2 or 3 gender articles.
 
-var gender_articles = [
-  'agender', 'boy', 'enby', 'epicine', 'girl', 'man', 'neutrois',
-  'gender-questioning', 'queer', 'woman',
+var gender_roots = [
+  {gen:'agender', pron: 'they/them'}, {gen:'boy', pron: 'he/him'},
+  {gen:'enby', pron: 'they/them'}, {gen:'epicine', pron: 'they/them'},
+  {gen:'girl', pron: 'she/her'}, {gen:'man', pron: 'he/him'},
+  {gen:'nuetrois', pron: 'they/them'},
+  {gen:'gender-questioning', pron: 'they/them'},
+  {gen:'queer', pron: 'they/them'},
+  {gen:'woman', pron: 'she/her'},
 ];
 /* I have left out cultural genders that are not a part of my culture, as that
 * I intend to use this for storytelling, and do not feel comfortable co-opting
@@ -15,20 +20,14 @@ var gender_articles = [
 * and above all respect would be nescisary, as such I don't think it's
 * appropriate to include in a random generator.
 */
-// TODO: it would be great to have pronoun tags related to gender article, in
-// order to pursue this goal, I will want to make gender_artciles objects with
-// a gender noun, and a weight_pronoun. Then go for a 50%/50% chance the weighted
-// pronoun is used.
-//
 // TODO: Add switch to include genders from other cultures, to avoid exclusion,
 // such as Hijra, Two-spirit, and other Third genders.
 
 var pronouns = [
-  'ey/em', 'gein/gein', 'he/him', 'one/one', 'she/her', 'sie/sie', 'tey/ter',
+  'ey/em', 'gein/gein', 'he/him', 'she/her', 'sie/sie', 'tey/ter',
   'they/them','ve/ver', 'zie/zim',
 ];
 // Note: gein is a gender nuetral pronoun in my storytelling used by dragons
-// Note: Honestly, I'd love to weigh these pronouns, based on gender_article
 // TODO: gein switch.
 
 var attraction_descriptor = [
@@ -45,8 +44,20 @@ function _randomizer(array){
   return  array[Math.floor(Math.random()* array.length)];
 }
 
+function flipcoin() {
+  var coin = [0,1];
+  var flip = _randomizer(coin);
+  if (flip === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function generate_gender() {
-  return _randomizer(gender_descriptors) + ' ' + _randomizer(gender_articles);
+  var root = _randomizer(gender_roots);
+  var descriptor = _randomizer(gender_descriptors);
+  return {rt: root, desc: descriptor};
 }
 
 function generate_attraction() {
@@ -56,10 +67,21 @@ function generate_attraction() {
     _randomizer(tempattraction_sex) + 'sexual';
 }
 
+function generate_pronoun() {
+  return _randomizer(pronouns);
+}
+
 
 function generate_personality() {
   var attraction = generate_attraction();
-  var pronoun = generate_pronoun();
   var gender = generate_gender();
-  return gender + ' with ' + pronoun + ' pronouns, who is ' + attraction;
+  var prnoun = '';
+  if (flipcoin()) {
+    pronoun = gender.rt.pron;
+  } else {
+    pronoun = generate_pronoun();
+  }
+  return gender.desc + ' ' + gender.rt.gen + ' with ' + pronoun + ' pronouns, who is ' + attraction;
 }
+
+generate_personality();
