3ca8e69868da8b0782cdbc468a85408ff00b617a
Kaylotura@gmail.com <Kaylotura@gmail.com>
2018-05-22 12:07:23 -0700

    added ancestry generator functions

--- ancestry_generator.js
+++ ancestry_generator.js
@@ -1,6 +1,5 @@
 //The goal of this module is to create vibrant ancestry profiles for nonplayer characters.
-
-// import { randomizer, flipcoin } from './randomizers.js';
+import { randomizer, dieRoll } from './randomizers.js';
 
 var ancestryObjects = [
   {
@@ -23,7 +22,7 @@ var ancestryObjects = [
     ]
   },
   {
-    rt: 'Elves',
+    rt: 'Elf',
     subs: [
       'Deep', 'High', 'Gold', 'Grey', 'Wood', 'Winged',
     ]
@@ -105,3 +104,21 @@ var templates = [
   'Therianthrope',
   'Vampire',
 ];
+
+// this function just resturns a constrcuted ancestry string from random
+// ancestryObject and a random subrace from the ancestryObject, with a 10% chance
+// of returning a templated ancestry.
+function generateAncestry() {
+  var ancestryObject =  randomizer(ancestryObjects);
+  var ancestryRoot = ancestryObject.rt;
+  var ancestrySub = randomizer(ancestryObject.subs);
+  var tenPercent = dieRoll(10);
+  if (tenPercent === 10) {
+    var templateSub = randomizer(templates);
+    return templateSub + " (" + ancestrySub + ") " + ancestryRoot;
+  } else {
+    return ancestrySub + " " + ancestryRoot;
+  }
+}
+
+generateAncestry();
