ae891778f7d610eb41c0c45abe5c2345bae266c4
Kaylotura@gmail.com <Kaylotura@gmail.com>
2018-07-20 20:52:46 -0700

    updated randomizer with a new randomCount function

--- randomizers.js
+++ randomizers.js
@@ -7,6 +7,25 @@ function randomizer(array){
   return  array[Math.floor(Math.random()* array.length)];
 }
 
+// Takes in an array and a value, and returns a new array of distinct random
+// values from the original array. This new array has a length equal to the value.
+// TODO: Have some logic if num > originalArray.length
+function randomizerCount(originalArray, num){
+  var resultBasket = [];
+  var itemBasket = [];
+  var catalogue = [...Array(originalArray.length).keys()];
+  var i;
+  for (i = 0; i < num; i++) {
+    var randomItem = randomizer(catalogue);
+    itemBasket.push(catalogue[randomItem]);
+    catalogue.filter(i => i != randomItem);
+  }
+  itemBasket.forEach(indexPoint =>
+    resultBasket.push(originalArray[indexPoint]));
+    return resultBasket;
+  }
+
+
 // Uses randomizer to randomly return true or false.
 function flipCoin() {
   var coin = [0,1];
