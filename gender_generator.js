661bf1b634b2b7a1d109bf7bc3881f2d96162e45
Kaylotura@gmail.com <Kaylotura@gmail.com>
2018-07-20 22:14:47 -0700

    in the middle of editing gender_generator for JSON migration

--- gender_generator.js
+++ gender_generator.js
@@ -1,14 +1,35 @@
 "use strict";
 //The goal of this module is to create vibrant multifacited gender profiles.
 
+/* I have left out cultural genders that are not a part of my culture, as that
+* I intend to use this for storytelling, and do not feel comfortable co-opting
+* other cultures as part of my storytelling. Research, caution, intenitonality,
+* and above all respect would be nescisary, as such I don"t think it"s
+* appropriate to include in a random generator.
+*/
+// TODO: Add switch to include genders from other cultures, to avoid exclusion,
+// such as hijra, two-spirit, and other third genders.
+// Note: gein is a gender nuetral pronoun in my storytelling used by dragons
+// TODO: gein switch.
+// TODO: Increase likelyhood of gein as pronoun if draconic
 
 // Returns a complex gender object.
 // Example: {rt: {gen:'girl', pron: 'she/her'}, desc: ['androgynous'] }
 // TODO clean this the heck up.
 function generate_gender() {
-  var root = randomizer(gender_roots);
-  var descriptor = randomizer(gender_descriptors);
-  return {rt: root, desc: descriptor};
+    var fivePercent = dieRoll(20);
+    if (fivePercent == 19) {
+        var descriptor = 'bigender';
+        var roots = randomizerCount(genderJSON.genderRoots, 2)
+    } else if (fivePercent == 20) {
+      var descriptor = 'pangender';
+      var numberOfGenders = dieRoll(4)
+      var roots = randomizerCount(genderJSON.genderRoots, numberOfGenders)
+    } else {
+      var descriptor = randomizer(genderJSON.genderDescriptors);
+      var roots = randomizer(genderJSON.genderRoots);
+    }
+  return {rt: roots, desc: descriptor};
 }
 
 // Retruns a string which describes a persons romantic and sexual attractions.
