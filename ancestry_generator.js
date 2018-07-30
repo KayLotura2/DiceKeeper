dec52f51f41f7508db2f085552e39332cfbfd2fc
Kaylotura@gmail.com <Kaylotura@gmail.com>
2018-07-30 11:04:05 -0700

    Fixed redundant templates, tidied some small errors

--- ancestry_generator.js
+++ ancestry_generator.js
@@ -14,12 +14,17 @@ function generateAncestry() {
     var newSize = "";
     var newTypes = [""];
     var beast = "";
-    var templateObj = randomizer(ancestryJSON.templates);
-    if (templateObj.newSize) {
-      newSize = templateObj.newSize + " ";
+    var templateBasket = randomizerCount(ancestryJSON.templates, 2);
+    var templateObj = templateBasket[0];
+    if (ancestryObj.subTypes[0] == templateObj.newsubTypes[0]) {
+      templateObj = templateBasket[1]
+      console.log("Using the second template!")
     }
-    if (templateObj.newnTypes) {
-      newTypes = ", "+ templateObj.newTypes.join(", ") + " ";
+    if (templateObj.newSize) {
+      newSize = `${templateObj.newSize} `;
+    }
+    if (templateObj.typeChange) {
+      newTypes = `, ${templateObj.typeChange} `;
     }
     if (templateObj.beasts) {
     var beastChoice = randomizer(templateObj.beasts);
@@ -30,11 +35,11 @@ function generateAncestry() {
       join("/")} ${newTypes} (${ancestryObj.subTypes.
       join(", ")}, ${templateObj.newsubTypes.
       join(", ")})`,
-      ancestry:  `${beast}${templateObj.name} ${ancestryObj.rt}${beast}`
+      ancestry:  `${beast}${templateObj.name} ${ancestryObj.rt}`
     };
   } else {
     return {
-      types: `${ancestrySubObj.size} ${ancestryObj.rt} (${ancestryObj.subTypes.join(", ")})`,
+      types: `${ancestrySubObj.size} ${ancestryObj.types.join("/")} (${ancestryObj.subTypes.join(", ")})`,
       ancestry: `${ancestrySubObj.name} ${ancestryObj.rt}`
     }
   }
