097d7f422b25fa6f34d62eff55a5abb4388d3883
Hekate V. Nichols <36681220+KayLotura2@users.noreply.github.com>
2020-01-10 22:23:05 -0800

    Working of JSONvalidation bug

--- jsonvalidate.js
+++ jsonvalidate.js
@@ -1,9 +1,16 @@
 var Ajv = require('ajv');
 const schema = require("./JSON/schema.json")
 const ancestry = require("./JSON/ancestry.json")
+var schemaNames = Object.keys(schema)
+let schemasArray = []
 
-var ajv = new Ajv();
-var validate = ajv.compile(schema["ancestries-schema"]);
-var valid = validate(ancestry);
-if (!valid) console.log(validate.errors);
-if(valid) console.log('IT WORKED EUREKA')
+for (let i = 0; i < schemaNames.length - 1; i++) {
+  schemasArray.push(schema[`${schemaNames[i]}`])
+}
+console.log(schemasArray)
+
+var ajv = new Ajv({schemas: schemasArray});
+// var validate = ajv.compile(schema["ancestries-schema"]);
+// var valid = validate(ancestry);
+// if (!valid) console.log(validate.errors);
+// if(valid) console.log('IT WORKED EUREKA')
