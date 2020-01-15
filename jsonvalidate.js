8a0ed072a2c3029d6b7f6a2c004d9258d60ddc3f
Hekate V. Nichols <36681220+KayLotura2@users.noreply.github.com>
2020-01-14 17:44:37 -0800

    still futzing with schema validate

--- jsonvalidate.js
+++ jsonvalidate.js
@@ -2,15 +2,26 @@ var Ajv = require('ajv');
 const schema = require("./JSON/schema.json")
 const ancestry = require("./JSON/ancestry.json")
 var schemaNames = Object.keys(schema)
-let schemasArray = []
 
-for (let i = 0; i < schemaNames.length - 1; i++) {
-  schemasArray.push(schema[`${schemaNames[i]}`])
-}
-console.log(schemasArray)
+const ajv = new Ajv({
+  allErrors: true,
+  extendRefs: 'fail',
+  verbose: true,
+  schemas: schema
+});
 
-var ajv = new Ajv({schemas: schemasArray});
+// for (let i = 0; i < schemaNames.length - 1; i++) {
+//   let schemaName = schemaNames[i]
+//   ajv.addSchema(schema[`${schemaName}`], schemaName)
+// }
+console.log(schemaNames)
+
+
+// var ajv = new Ajv({schemas: schemasArray});
 // var validate = ajv.compile(schema["ancestries-schema"]);
 // var valid = validate(ancestry);
 // if (!valid) console.log(validate.errors);
-// if(valid) console.log('IT WORKED EUREKA')
+
+console.log(ajv.schemas)
+
+console.log(ajv.validate('ancestries-schema', ancestry), ajv.errors);
